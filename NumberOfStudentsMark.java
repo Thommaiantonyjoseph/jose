13df0959cd7fb3c6ac759a0f7335ac59c031a004
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2016-11-24 08:52:04 +0530

    Add files via upload

---
+++ NumberOfStudentsMark.java
@@ -0,0 +1,49 @@
+package Nov9;
+
+import java.util.Scanner;
+
+public class NumberOfStudentsMark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	NumberOfStudentsMark in=new NumberOfStudentsMark();
+	Scanner jose=new Scanner(System.in);
+
+
+int student=0;    
+while(student<5){
+	int subject=0;
+         String studentsnames;
+         student=student+1;
+         subject=subject+1;
+        System.out.println("Enter student names="+ "");
+        String studentsnames1=jose.nextLine();
+        String studentsnames2=jose.nextLine();
+       
+      System.out.println("enter mark=");
+       int Mark1=jose.nextInt();
+       int Mark2=jose.nextInt();
+       int Mark3=jose.nextInt();
+       int Mark4=jose.nextInt();
+       int Mark5=jose.nextInt();
+    int total= in.add(Mark1,Mark2,Mark3,Mark4,Mark5);
+   float avg= in.avg(total);
+    System.out.println("Total is="+ in.add(Mark1,Mark2,Mark3,Mark4,Mark5));
+    System.out.println("avg is="+in.avg(total));
+     } 
+	}
+
+	private float avg(int total) {
+		// TODO Auto-generated method stub
+		float avg=(total)/5;
+		return avg;
+	}
+
+	private int add(int a,int b,int c,int d,int e) {
+		
+		// TODO Auto-generated method stub
+		int total=(a+b+c+d+e);
+		return total;
+	}
+
+}
