db2b8c74c83eb02d9a16b71fd8583d63c2965e6e
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2016-11-26 08:27:11 +0530

    Add files via upload
    use for char

--- Calculate.java
+++ Calculate.java
@@ -11,32 +11,39 @@ public class Calculate {
 		 do{
 	      int a ;
 	      float result;
+	     
 	      Scanner joseph=new Scanner(System.in);
+	     
 	      System.out.println("1.Addition");
 	      System.out.println("2.Subraction");
 	      System.out.println("3.Multiply");
 	      System.out.println("4.division");
 	      System.out.println(" Enter your choice=");
-	      int choice=joseph.nextInt();
+	    //  int choice=joseph.nextInt();
+	      
+	      char check;
+	      
+	      check=joseph.next().charAt(0);
+	     // System.out.println("u enter"+check);
 	      System.out.println("Enter a value");
 	      a=joseph.nextInt();
 	      System.out.println("Enter b value");
 	      b=joseph.nextInt();
-	      switch(choice){
-	      case 1:
+	      switch(check){
+	      case'+':
 	    	  float add=a+b;
 	    	  System.out.println("1.Addition Value is="+add);
 	    	  break;
-	      case 2:
+	      case '-':
 	    	  float sub=a-b;
 	    	  System.out.println("2.Subraction Value is="+sub);
 	    	  break;
-	      case 3:
+	      case '*':
 	    	  float mul=a*b;
 	    	  System.out.println("3.Multiply is="+mul);
 	    	  break;
-	      case 4:
-	    	  float div=a+b;
+	      case '/':
+	    	  float div=a/b;
 	    	  System.out.println("4.division Value is="+div);
 	    	  break;
 	    	  
