b0550b33b68da16493a582f7f12ed6966f8c4e7f
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2016-11-28 15:07:58 +0530

    Add files via upload

---
+++ AddingDigit.java
@@ -0,0 +1,27 @@
+package Com.Payilagam.Java;
+
+import java.util.Scanner;
+
+public class AddingDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner joseph= new Scanner(System.in);
+int a;
+int reverse=0;
+System.out.println("Please Enter Your  reverse number");
+a=joseph.nextInt();
+while(a>0){
+	reverse=a%10;
+	
+	a=a/10;
+	
+
+
+
+
+	System.out.print(reverse);
+	
+	}}}
+
+
