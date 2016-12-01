abfaa953bc580915317e72c589c3284f4dc2d5b9
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2016-12-01 15:11:47 +0530

    Add files via upload

---
+++ AscendingOrder.java
@@ -0,0 +1,25 @@
+package Com.Payilagam.Java;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=0;i<=0;i++){
+			
+			for(int j=1;j>i;j--){
+				System.out.print("1\n");
+				System.out.println("22");
+			}
+			for(int k=0;k<=0;k++){
+			System.out.print("333\n");	
+			System.out.println("4444");
+			System.out.println("55555");
+			}
+			System.out.println();
+			
+		}
+			
+		}
+	}
+
+
