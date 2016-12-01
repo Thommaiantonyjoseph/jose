a0d852c52c4204d862314768e44dafe309dfdfac
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2016-12-01 14:18:07 +0530

    Add files via upload

---
+++ StarDiamond.java
@@ -0,0 +1,42 @@
+package Com.Payilagam.Java;
+
+public class StarDiamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=0;i<4;i++){
+			
+			for (int j=5;j>i;j--){
+				
+			
+			System.out.print(" ");
+			}	
+			for(int k=0;k<=i;k++){
+					
+					System.out.print("* ");
+					
+				}
+
+				System.out.println();
+}
+			{
+			for (int i=4;i>=0;i--){
+				
+				for (int j=5;j>i;j--){
+					
+				
+				System.out.print(" ");
+				}	
+				for(int k=0;k<=i;k++){
+						
+						System.out.print("* ");
+						
+					}
+
+					System.out.println();
+			
+			}
+		}
+	}
+
+}
