3614fa90d64a821ff8f05f7edac0f6bea61fba6d
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2017-01-12 14:15:09 +0530

    Add files via upload

---
+++ Anonymousinnerclass.java
@@ -0,0 +1,27 @@
+package Com.java.payilagam;
+
+public abstract class Anonymousinnerclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+	Anonymousinnerclass obj=new Anonymousinnerclass(){
+		
+		void add(){
+			int a=5;
+			int b=9;
+			System.out.println(a+b);
+		}
+
+	
+		
+		
+		
+		
+	};
+	obj.add();
+	}
+	abstract void add();
+}
