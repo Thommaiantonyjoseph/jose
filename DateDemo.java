da67b618227c322a1a77df7816da7aec149df7be
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2017-01-26 11:47:49 +0530

    Add files via upload

---
+++ DateDemo.java
@@ -0,0 +1,16 @@
+package Com.Java;
+
+import java.util.Date;
+
+public class DateDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Date obj=new Date(); 
+ 
+
+System.out.println(obj.toString());
+
+	}
+
+}
