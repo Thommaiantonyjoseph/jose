c475be8e7ec531c82e6ba80811f80f2f5b96ebeb
Thommaiantonyjoseph <antonyjoseph14313@gmail.com>
2018-04-11 18:32:03 +0530

    Add files via upload

---
+++ ACS.java
@@ -0,0 +1,20 @@
+package com.java.joe;
+
+import java.util.Scanner;
+
+public class ACS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Scanner obj=new Scanner(System.in);
+		//System.out.println("Enter your character");
+		char ch='a';
+		int askey=(int)ch;
+		System.out.println(askey);
+		
+		
+	}
+
+}
